656482a90782d27bfd884604a7cf88846f4b8a2a
Dilshan Ramesh <dilshanramesh81@gmail.com>
2023-08-09 02:41:33 +0530

    item card ui fixes

--- lib/screens/widgets/item.dart
+++ lib/screens/widgets/item.dart
@@ -1,6 +1,7 @@
 import 'package:coffee_app/common/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
 
 class Item extends StatefulWidget {
   final String image;
@@ -18,92 +19,135 @@ class _ItemState extends State<Item> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 239,
-        width: 150,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(16),
+      height: 239,
+      width: 150,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 13,
+          right: 13,
         ),
-        child: Padding(
-          padding: const EdgeInsets.only(
-            left: 13,
-            right: 13,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: 142,
-                height: 132,
-                alignment: Alignment.center,
-                child: Image.asset(
-                  widget.image,
-                  fit: BoxFit.contain,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Container(
                   width: 142,
-                ),
-              ),
-              GestureDetector(
-                onTap: () => Navigator.pushNamed(
-                  context,
-                  Routes.detail,
-                ),
-                child: Text(
-                  "Cappucino",
-                  style: GoogleFonts.sora(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                    color: const Color(0xff2F2D2C),
+                  height: 132,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(widget.image),
+                      fit: BoxFit.fitWidth,
+                      alignment: Alignment.center,
+                      scale: 1
+                    ),
                   ),
                 ),
+                Positioned(
+                  top: 12,
+                  left: -1,
+                  child: Container(
+                    width: 51,
+                    height: 25,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent.withOpacity(0.5),
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        bottomRight: Radius.circular(16),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Iconsax.star1,
+                          color: Color(0xfffFBBE21),
+                        ),
+                        const SizedBox(
+                          width: 2,
+                        ),
+                        Text(
+                          "4.9",
+                          style: GoogleFonts.sora(
+                            fontSize: 10,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            GestureDetector(
+              onTap: () => Navigator.pushNamed(
+                context,
+                Routes.detail,
               ),
-              const SizedBox(
-                height: 4,
-              ),
-              Text(
-                'with Chocolate',
+              child: Text(
+                "Cappucino",
                 style: GoogleFonts.sora(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w400,
-                  color: const Color(0xff9B9B9B),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: const Color(0xff2F2D2C),
                 ),
               ),
-              const SizedBox(
-                height: 11,
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(
+              'with Chocolate',
+              style: GoogleFonts.sora(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: const Color(0xff9B9B9B),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "\$ 4.53",
-                    style: GoogleFonts.sora(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      color: const Color(0xff2F4B4E),
+            ),
+            const SizedBox(
+              height: 11,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "\$ 4.53",
+                  style: GoogleFonts.sora(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0xff2F4B4E),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => Navigator.pushNamed(
+                    context,
+                    Routes.detail,
+                  ),
+                  child: Container(
+                    width: 32,
+                    height: 32,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffC67C4E),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.white,
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () => Navigator.pushNamed(
-                      context,
-                      Routes.detail,
-                    ),
-                    child: Container(
-                      width: 32,
-                      height: 32,
-                      decoration: BoxDecoration(
-                        color: const Color(0xffC67C4E),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: const Icon(
-                        Icons.add,
-                        color: Colors.white,
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
+                )
+              ],
+            )
+          ],
         ),
-      );
+      ),
+    );
   }
 }
